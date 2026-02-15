161f72de6b4cf167af25b7023c4c4090b0c6dc68
stelo <254087305+stelo-web@users.noreply.github.com>
2026-02-15 20:31:07 +0900

    Add service worker for font caching

---
+++ v2/sw.js
@@ -0,0 +1,31 @@
+// sw.js
+const CACHE_NAME = 'font-cache-v1';
+const FONT_URL = 'https://stelo-web.github.io/main/font/Noto_Sans_JP.zip/NotoSansJP-VariableFont_wght.ttf';
+
+self.addEventListener('install', (event) => {
+    // Service Workerインストール時にフォントをキャッシュ
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.add(FONT_URL);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    // フォントへのリクエストならキャッシュから返す
+    if (event.request.url === FONT_URL) {
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                return response || fetch(event.request).then((response) => {
+                    return caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    });
+                });
+            })
+        );
+    } else {
+        // それ以外は通常通り
+        event.respondWith(fetch(event.request));
+    }
+});
